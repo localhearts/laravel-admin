0ef0ada8ca0b0df2d64ccdee5ffc54370ec4c6eb
Who Am I <localhearts@Whos-MacBook-Pro.local>
2023-09-27 20:45:26 +0900

    update pekerjaan 26/09/2023

--- app/Imports/ImportAttendance.php
+++ app/Imports/ImportAttendance.php
@@ -3,22 +3,32 @@
 namespace App\Imports;
 
 use App\Models\Attendance;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class ImportAttendance implements ToModel
+class ImportAttendance implements ToModel, WithStartRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+    public function startRow(): int
+    {
+        return 2;
+    }
+    
     public function model(array $row)
     {
         return new Attendance([
             //
             'employee_id' => $row[0],
-            'check_in' => $row[1],
-            'check_out' => $row[2],
+            'check_in' => Carbon::createFromFormat('d/m/Y H:i',$row[1])->format('Y-m-d H:i'),
+            'check_out' => Carbon::createFromFormat('d/m/Y H:i',$row[2])->format('Y-m-d H:i'),
         ]);
+       //
+       
+
     }
 }
--- app/Imports/ImportEmployee.php
+++ app/Imports/ImportEmployee.php
@@ -4,14 +4,21 @@ namespace App\Imports;
 
 use App\Models\Employee;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class ImportEmployee implements ToModel
+class ImportEmployee implements ToModel, WithStartRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+    
+    public function startRow(): int
+    {
+        return 2;
+    }
+
     public function model(array $row)
     {
         return new Employee([
